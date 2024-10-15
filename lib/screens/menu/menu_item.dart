fc88fde10b3dd62e13bca1506be10117b81694a5
prime-infinity <soe.a.prime@gmail.com>
2024-10-15 14:30:02 -0700

    working on menuitem component

---
+++ lib/screens/menu/menu_item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MenuItem extends StatelessWidget {
+  const MenuItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}

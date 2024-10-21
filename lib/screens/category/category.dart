ea2d6849bfcfbf852422c491fbc800c7d4f4d77e
prime-infinity <soe.a.prime@gmail.com>
2024-10-20 17:18:49 -0700

    added animations

--- lib/screens/category/category.dart
+++ lib/screens/category/category.dart
@@ -9,30 +9,24 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          border: Border.all(
-        color: Colors.black,
-      )),
-      child: Column(
-        //this column is for the main categories
-        //we do it this way so we can display the
-        //categoty name first and then the menu items after
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(category.name),
-          const SizedBox(
-            height: 5,
-          ),
-          ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: category.menuItems.length,
-              itemBuilder: (context, index) {
-                return MenuItemCard(category.menuItems[index]);
-              }),
-        ],
-      ),
+    return Column(
+      //this column is for the main categories
+      //we do it this way so we can display the
+      //categoty name first and then the menu items after
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(category.name),
+        const SizedBox(
+          height: 5,
+        ),
+        ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: category.menuItems.length,
+            itemBuilder: (context, index) {
+              return MenuItemCard(category.menuItems[index]);
+            }),
+      ],
     );
   }
 }
--- lib/screens/category/menu_item.dart
+++ lib/screens/category/menu_item.dart
@@ -12,77 +12,94 @@ class MenuItemCard extends StatefulWidget {
 
 class _MenuItemCardState extends State<MenuItemCard> {
   int _currentImageIndex = 0;
+  bool isTapped = false;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       //now we are in a menu item
       margin: const EdgeInsets.symmetric(vertical: 8),
-      decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
       child: Column(
         //we create two colums for the up
         //and down, the down will contain
         //the item images as horizontal scroll in container
         children: [
           IntrinsicHeight(
-            child: Row(
-              //this row is where the item image
-              //name,price and description will be
-              //the name,price and description will be
-              // the second item on the row
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  //image container
-                  width: 128,
-                  height: 128,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.yellow),
-                    image: DecorationImage(
-                      image: AssetImage(
-                          "assets/images/foods/${widget.menuItem.images.first}"),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: Container(
-                    //remove this container later
-                    //price, name and description container
-                    decoration:
-                        BoxDecoration(border: Border.all(color: Colors.green)),
-                    padding: const EdgeInsets.all(8),
-                    child: Column(
-                      //this will have two columns, the first will be
-                      //a rowfor name and price, and the second will be for
-                      //description
-                      crossAxisAlignment: CrossAxisAlignment.start,
-
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(widget.menuItem.name),
-                            Text("\$${widget.menuItem.price}"),
-                          ],
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isTapped = !isTapped;
+                });
+              },
+              child: Row(
+                //this row is where the item image
+                //name,price and description will be
+                //the name,price and description will be
+                // the second item on the row
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  AnimatedSize(
+                    duration: const Duration(milliseconds: 400),
+                    curve: Curves.easeInOut,
+                    child: Container(
+                      //image container
+                      width: isTapped ? 0 : 128,
+                      height: 128,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        image: DecorationImage(
+                          image: AssetImage(
+                              "assets/images/foods/${widget.menuItem.images.first}"),
+                          fit: BoxFit.cover,
                         ),
-                        const Spacer(),
-                        Text(widget.menuItem.description),
-                      ],
+                      ),
                     ),
                   ),
-                )
-              ],
+                  // Use AnimatedContainer for spacing between widgets
+                  AnimatedContainer(
+                    duration: const Duration(milliseconds: 400),
+                    width: isTapped ? 0 : 8,
+                  ),
+                  Expanded(
+                    child: Container(
+                      //remove this container later
+                      //price, name and description container
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.transparent)),
+                      padding: const EdgeInsets.all(8),
+                      child: Column(
+                        //this will have two columns, the first will be
+                        //a rowfor name and price, and the second will be for
+                        //description
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(widget.menuItem.name),
+                              Text("\$${widget.menuItem.price}"),
+                            ],
+                          ),
+                          const Spacer(),
+                          Text(widget.menuItem.description),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
           if (widget.menuItem.images.length > 1)
             Column(children: [
               //the container that has the images
-              Container(
-                height: 371,
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 400),
+                height: isTapped ? 371 : 0,
                 decoration:
-                    BoxDecoration(border: Border.all(color: Colors.blue)),
+                    BoxDecoration(border: Border.all(color: Colors.black)),
                 child: Stack(children: [
                   PageView.builder(
                       scrollDirection: Axis.horizontal,
@@ -95,7 +112,7 @@ class _MenuItemCardState extends State<MenuItemCard> {
                       itemBuilder: (context, index) {
                         return Container(
                           decoration: BoxDecoration(
-                            border: Border.all(color: Colors.green),
+                            border: Border.all(color: Colors.black),
                             image: DecorationImage(
                               image: AssetImage(
                                   'assets/images/foods/${widget.menuItem.images[index + 1]}'),

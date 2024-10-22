92612199ae6f51141df473f22c1c6d0ad09626e6
prime-infinity <soe.a.prime@gmail.com>
2024-10-22 11:24:48 -0700

    added horizontal list categoty buttons

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,44 @@ class SandBox extends StatelessWidget {
         title: const Text("sandbox"),
         backgroundColor: const Color.fromARGB(255, 220, 154, 232),
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: ListView.builder(
-            itemCount: categories.length,
-            itemBuilder: (context, index) {
-              return CategoryCard(categories[index]);
-            }),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.white,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: categories.map((cat) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextButton(
+                        style: TextButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            padding: const EdgeInsets.symmetric(horizontal: 26),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(0))),
+                        onPressed: () {},
+                        child: Text(
+                          cat.name,
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 16),
+                        )),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: ListView.builder(
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    return CategoryCard(categories[index]);
+                  }),
+            ),
+          ),
+        ],
       ),
     );
   }
